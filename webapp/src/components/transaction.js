355ccd6e4d1a5662afa614f281accebed5aee8fd
Tiffany Fritz <tfritz@nav.com>
2019-07-14 23:54:43 -0600

    add icons

--- webapp/src/components/transaction.js
+++ webapp/src/components/transaction.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import styled from '@emotion/styled'
+import { FaTrashAlt, FaEdit } from 'react-icons/fa'
 
 const TransactionWrapper = styled.div`
   border-top: 1px solid #D0D2D6;
@@ -7,6 +8,15 @@ const TransactionWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+`
+
+const Delete = styled(FaTrashAlt)`
+  color: #D0D2D6;
+  cursor: pointer;
+
+  &:hover {
+    color: #CF2F34;
+  }
 `
 
 const Copy = styled.p`
@@ -24,14 +34,25 @@ const TransactionMerchant = styled(Copy)``
 
 const TransactionCategory = styled(Copy)``
 
+const Edit = styled(FaEdit)`
+  color: #D0D2D6;
+  cursor: pointer;
+
+  &:hover {
+    color: #00BB8B;
+  }
+`
+
 export const Transaction = () => {
   return (
     <Fragment>
       <TransactionWrapper>
+        <Delete />
         <TransactionDate>6/12/19</TransactionDate>
         <TransactionAmount>$100.00</TransactionAmount>
         <TransactionMerchant>Anthropologie</TransactionMerchant>
         <TransactionCategory>Shopping</TransactionCategory>
+        <Edit />
       </TransactionWrapper>
     </Fragment>
   )
